b39d7dade5b0d2f6457b4200efe2ee332f1f00bc
Juliana Soares <julianaso840@gmail.com>
2025-03-18 13:08:39 -0300

    Botão de cadastrar salvando os produtos

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -10,7 +10,10 @@ import javax.swing.JOptionPane;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Adm
@@ -18,16 +21,20 @@ import javax.swing.JOptionPane;
 public class conectaDAO {
     
     public Connection connectDB(){
-        Connection conn = null;
-        
+         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/produtos",  
+                    "root",  
+                    "@Juli4321"); 
+        return conn;
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            return null;
         }
-        return conn;
+         
     }
     
 }
